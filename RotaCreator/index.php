93463057a63f31cf16739068e9cb31000846081e
Orbi76bit <oorbangabor@gmail.com>
2020-01-06 23:12:26 +0000

    Start

---
+++ RotaCreator/index.php
@@ -0,0 +1,41 @@
+<?php
+
+include_once("navbar.php");
+
+?>
+<head>
+    <meta charset="utf-8"/>
+    <title> Index </title>
+
+    <!-- Latest compiled and minified CSS -->
+<!--    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+-->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+</head>
+
+
+<body>
+
+<h1>Welcome on the Rota Creator site. </h1>
+<h2>Dear Visitor! </h2>
+<h2>The application is not fully developed yet.</h2>
+
+<h2>Thank you for your understanding.</h2>
+<p> The Add Employee function is working perfectly and can add an employee to the database with the filled out details.</p>
+<p> In the rota tags the starting date of the rota can be selected and then the selected day will be the first day in the table.</p>
+<p> If you click on the Choose option  on the employees list, the name of the employee will appears in the table.</p>
+
+    <button class="btn btn-success" onclick="$(this).hide();"> Click me!</button>
+
+
+
+
+
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
---
+++ RotaCreator/weben.php
@@ -0,0 +1,3 @@
+<?php
+echo "webes githubban";
+?>
